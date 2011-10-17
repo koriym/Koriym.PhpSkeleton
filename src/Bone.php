9816148bae1a5b1d4e30249505562d7ca3affb6a
koriym <akihito.koriyama@gmail.com>
2011-10-17 23:45:12 +0900

    Change coment.

--- src/Bone.php
+++ src/Bone.php
@@ -1,10 +1,18 @@
 <?php
 /**
- * This file is part of the PHP.Bone Project for PHP.
+ * PHP.Bone
  * 
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
 namespace PHP\Bone;
 
-class Bone {}
+/**
+ * Bone
+ * 
+ * @package PHP.Bone
+ * @author  
+ */
+class Bone 
+{
+}
--- src/Exception.php
+++ src/Exception.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the PHP.Bone Project for PHP.
+ * PHP.Bone
  * 
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
