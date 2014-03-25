a59f5b8326061e2969ddbe7151c12c3049d29175
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-03-25 16:39:33 +0900

    psr4 layout

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,4 @@
 
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /** @var $loader \Composer\Autoload\ClassLoader */
-$loader->addPsr4('Php\Skeleton\\', __DIR__);
+$loader->addPsr4('PHP\Skeleton\\', __DIR__);
