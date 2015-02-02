e43d48f382d33074cdb1ac69341fa35843a2d87c
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-02-02 20:41:11 +0900

    put @var previous line

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /** @var $loader \Composer\Autoload\ClassLoader */
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
