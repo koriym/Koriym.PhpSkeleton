d92048b3239f8ae9e1163d2b05b2f244275884e4
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-01-14 11:14:07 +0900

    remove phpdoc

--- src/Exception/LogicException.php
+++ src/Exception/LogicException.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace __Vendor__\__Package__\Exception;
 
-/**
- *  Base Logic Exception class for __Vendor__.__Package__
- */
 class LogicException extends \LogicException
 {
 }
--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace __Vendor__\__Package__\Exception;
 
-/**
- *  Base Runtime Exception class for __Vendor__.__Package__
- */
 class RuntimeException extends \RuntimeException
 {
 }
